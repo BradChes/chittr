9140f67b517fd548953e73cf607e106cb4a3737a
Brad Chesworth <bradley.chesworth@bbc.co.uk>
2020-02-25 11:45:51 +0000

    Added PostScreen

---
+++ screens/PostScreen.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import { 
+    StyleSheet,
+    View,
+    Text,
+    AsyncStorage } from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1, 
+        justifyContent: 'center', 
+        alignItems: 'center'
+    }
+});
+
+export default class PostScreen extends Component {
+    constructor() {
+        super();
+        this.state = {
+            name: ''
+        };
+        this._readyUp();
+    }
+
+    _readyUp = async => {
+        const userName = await AsyncStorage.getItem('UserName');
+        this.setState({name: userName});
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text>Welcome {this.state.name}</Text>
+                <Text>to Post Screen</Text>
+            </View>
+        );
+    }
+}
