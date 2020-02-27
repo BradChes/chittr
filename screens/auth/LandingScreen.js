807aacfff801503b32c490b69f826a0510a511c7
Brad Chesworth <bradley.chesworth@bbc.co.uk>
2020-02-27 15:07:49 +0000

    Improved UI of LandingScreen

--- screens/auth/LandingScreen.js
+++ screens/auth/LandingScreen.js
@@ -4,6 +4,7 @@ import {
     View, 
     Button, 
     Text, 
+    Image,
     StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
@@ -12,25 +13,59 @@ const styles = StyleSheet.create({
         justifyContent: 'center', 
         alignItems: 'center'
     },
-    buttonTextDivider: {
-        marginVertical: 20,
-        fontSize: 18
+    headingContainer: {
+        flex: 3,
+        alignItems: 'center'
+    },
+    loginContainer: {
+        flex: 2,
+    },
+    signupContainer: {
+        flex: 1
+    },
+    header: {
+        fontSize: 44,
+        fontWeight: 'bold'
+    },
+    image: {
+        width: 150,
+        height: 150
+    },
+    slogan: {
+        fontSize: 18,
+        marginVertical: 20
+    },
+    signupPrompt: {
+        marginVertical: 10,
+        fontSize: 14
     }
 });
 
 export default class LandingScreen extends Component {
     render() {
         return (
-            <View style={styles.container}>
-                <Button 
-                    title = "Log In"
-                    color = 'red'
-                    onPress={() => this.props.navigation.navigate('Login')} />
-                <Text style = {styles.buttonTextDivider}>Or...</Text>
-                <Button 
-                    title = "Sign Up"
-                    color = 'red'
-                    onPress={() => this.props.navigation.navigate('Signup')} />
+            <View style = { styles.container }>
+                <View style = {styles.headingContainer}>
+                    <Text style = { styles.header }>Chittr</Text>
+                    <Image
+                        source = {require('./../../images/app-icon.png')}
+                        style = {styles.image}
+                    />
+                    <Text style = { styles.slogan }>What has happened?</Text>
+                </View>
+                <View style = {styles.loginContainer}>
+                    <Button 
+                        title = "Log In"
+                        color = 'red'
+                        onPress = {() => this.props.navigation.navigate('Login')} />
+                </View>
+                <View style = {styles.signupContainer}>
+                    <Text style = {styles.signupPrompt}>Otherwise...</Text>
+                    <Button 
+                        title = "Sign Up"
+                        color = 'red'
+                        onPress= {() => this.props.navigation.navigate('Signup')} />
+                </View>
             </View>
         );
     }
