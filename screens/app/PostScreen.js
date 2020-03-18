9215f615bcfc1f4aa38f2d4fab498b0ea42571ea
Brad Chesworth <bradley.chesworth@bbc.co.uk>
2020-03-18 17:21:11 +0000

    Disabled drafting if nothing has been entered in text field

--- screens/app/PostScreen.js
+++ screens/app/PostScreen.js
@@ -256,6 +256,7 @@ export default class PostScreen extends Component {
           />
 
           <ActionIcon
+            disabled={!enabled}
             onPress={() => this.saveDraft()}
             name='edit'
           />
